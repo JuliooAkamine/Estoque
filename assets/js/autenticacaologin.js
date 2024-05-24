0c783aaeadd96a577ba5dd8043124e3338230962
JuliooAkamine <julio.caleite@senacsp.edu.br>
2024-05-24 17:07:41 -0300

    cadastro e autenticação de usuarios feita

---
+++ assets/js/autenticacaologin.js
@@ -0,0 +1,61 @@
+//Resgatando array de usuarios no localstorage
+let usuariosJSON = localStorage.getItem("usuarios");
+let usuario = JSON.parse(usuariosJSON)
+console.log(usuario)
+
+// Autenticando Usuario
+const btnLogin = document.getElementById("btn_login")
+
+
+
+btnLogin.addEventListener("click", function(event){
+    event.preventDefault()
+let email = document.getElementById("user_email").value
+let password = document.getElementById("user_password").value
+
+if(email ===  "" || password === ""){
+    alert("Preencha todos os campos")
+}else{
+    // Verificar se existe um usuário com o email fornecido
+    let usuarioEncontrado = usuario.find(function(user) {
+    return user.emailUser === email;
+});
+
+    if (usuarioEncontrado) {
+        // Se o usuário for encontrado, verificar se a senha está correta
+        if (usuarioEncontrado.passwordUser === password) {
+            Swal.fire({
+                title: "Autenticação bem sucedida",
+                icon: "success"
+              });
+
+              setTimeout(() => {
+                window.location.replace("../pages/home.html")
+              }, 1000);
+    
+           
+        } else {
+            alert("Senha incorreta.");
+        }
+    } else {
+        alert("Email não cadastrado.");
+    }
+}
+
+
+
+
+});
+
+
+console.log(usuario.passwordUser)
+//Função para alterar a visibilidade do input de senha
+const btnVisibilityOFF = document.getElementById("visibility_off")
+btnVisibilityOFF.addEventListener("click", function(){
+    if(btnVisibilityOFF.src.endsWith("visibility_on.svg")){
+        btnVisibilityOFF.src = "../assets/icons/visibility_off.svg"
+        document.getElementById("user_password").type = "password"
+    } else {
+        btnVisibilityOFF.src = "../assets/icons/visibility_on.svg"
+        document.getElementById("user_password").type = "text"
+    }})
---
+++ assets/js/registrodeusuario.js
@@ -0,0 +1,65 @@
+const formCadastro = document.getElementById("register.form")
+const cadastrar = document.getElementById("btn_register")
+
+const btnVisibilityOFF = document.getElementById("visibility_off")
+const btnVisibilityOFF2 = document.getElementById("visibility_off2")
+
+//Função para alterar a visibilidade do input de senha
+btnVisibilityOFF.addEventListener("click", function(){
+    if(btnVisibilityOFF.src.endsWith("visibility_on.svg")){
+        btnVisibilityOFF.src = "../assets/icons/visibility_off.svg"
+        document.getElementById("user_password_register").type = "password"
+    } else {
+        btnVisibilityOFF.src = "../assets/icons/visibility_on.svg"
+        document.getElementById("user_password_register").type = "text"
+    }})
+
+
+//Função para alterar a visibilidade do input de confirmar senha 
+btnVisibilityOFF2.addEventListener("click",function(){
+    if(btnVisibilityOFF2.src.endsWith("visibility_on.svg")){
+        btnVisibilityOFF2.src = "../assets/icons/visibility_off.svg"
+        document.getElementById("user_password_confirmed").type = "password"
+    } else {
+        btnVisibilityOFF2.src = "../assets/icons/visibility_on.svg"
+        document.getElementById("user_password_confirmed").type = "text"
+    }
+})
+
+cadastrar.addEventListener("click", function(event){
+    const email = document.getElementById("user_email_register").value
+    const password = document.getElementById("user_password_register").value
+    const passwordConfirmed = document.getElementById("user_password_confirmed").value
+
+    if(email === "" || password === "" || passwordConfirmed === ""){
+        alert("preencha todos os campos")
+    }else{
+        if(password === passwordConfirmed){
+            var usuarios = localStorage.getItem('usuarios') ? JSON.parse(localStorage.getItem('usuarios')) : [];
+    
+            let usuario = {
+                emailUser: email,
+                passwordUser: password
+            }
+        
+            usuarios.push(usuario)
+            localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    
+            Swal.fire({
+                title: "Usuario cadastrado com sucesso",
+                icon: "success"
+              });
+    
+            console.log(usuario)
+        }else{
+            alert("Senha Incorreta")
+        }
+    }
+
+    event.preventDefault()
+
+    
+
+    
+})
+
