54d3cb69274236d22a5ca89f653a2bd6f1d17b03
Júlio Akamine <julio.caleite@senacsp.edu.br>
2024-05-28 17:03:12 -0300

    fazendo modal de registrar entrada

---
+++ assets/js/meuestoque.js
@@ -0,0 +1,9 @@
+const btnRegistrarEntrada = document.getElementById("register_demand")
+const btnRegistrarSaida = document.getElementById("add_demand")
+const btnAdicionarPedidos = document.getElementById("register_demand")
+
+const formRegistrarEntrada = document.getElementById("form_register_demand")
+
+btnRegistrarEntrada.addEventListener("click", function(){
+
+})
